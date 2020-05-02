5690a552e69e9aba29e59bdfd6d4de333c601695
Sebastian Kapunkt <kindt.Sebastian@outlook.com>
2020-05-02 14:30:29 +0200

    Add navigation highlighting based on page scroll

--- js/utils.js
+++ js/utils.js
@@ -12,3 +12,35 @@ const groupBooksByAttribute = (books, attribute) => {
 const sumPages = (books) => {
     return books.reduce((total, book) => total + book.pages, 0)
 }
+
+const hightlightNav = () => {
+    let navItems = document.getElementById("navigation").querySelectorAll('.nav-item')
+    
+    let anchorPositions = []
+    for (let i = 0; i < navItems.length; i++) {
+        navItems[i].classList.remove('nav-active')
+        anchorPositions.push(
+            [
+                navItems[i],
+                document.getElementById(navItems[i].getAttribute("href").replace("#","")).offsetTop
+            ]
+        )
+    }
+    for (let i = 0; i < anchorPositions.length; i++) {
+        if(
+            (
+                i === anchorPositions.length - 1
+                && document.documentElement.scrollTop >= anchorPositions[i][1]
+            )
+            || (
+                document.documentElement.scrollTop >= anchorPositions[i][1]
+                && document.documentElement.scrollTop < anchorPositions[i+1][1]
+            )
+        ){
+            anchorPositions[i][0].classList.add("nav-active")
+            history.pushState(null, null, anchorPositions[i][0].getAttribute("href"));
+        }
+    }
+}
+
+window.addEventListener('scroll', hightlightNav, false);
