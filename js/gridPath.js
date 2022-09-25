dede998da5ceae1a415897bac1a6b4f28a3d1f96
SebastianKapunkt <kindt.sebastian@outlook.com>
2022-09-25 21:42:11 +0200

    refactor to use classes and link points

--- js/gridPath.js
+++ js/gridPath.js
@@ -17,119 +17,197 @@ canvas.width = minSize * scale;
 canvas.height = minSize * scale;
 playgroundElement.appendChild(canvas)
 
-const ctx = canvas.getContext("2d");
-const strokeColor = 'white'
-const strokeActiveColor = '#fcbe24'
-ctx.strokeStyle = strokeColor;
-ctx.lineWidth = 4;
 
-const rows = 10
-const columns = 10
-const circleRadius = 8
-const length = Math.ceil(minSize / columns) * scale
-const gridWith = length / scale * (columns - 1)
-const gridPadding = (minSize - gridWith) / 2
-const offset = (minSize - gridWith - gridPadding) * scale
-
-for (let position = 1; position <= rows * columns; position++) {
-    drawCircle(getXCoordinate(position), getYCoordinate(position), '#444')
-}
-drawCircle(getXCoordinate(1), getYCoordinate(1), strokeActiveColor)
-
-function drawLine(from, to, color) {
-    ctx.strokeStyle = color;
-    const startX = getXCoordinate(from)
-    const startY = getYCoordinate(from)
-    const endX = getXCoordinate(to)
-    const endY = getYCoordinate(to)
-    ctx.beginPath()
-    ctx.moveTo(startX,startY)
-    ctx.lineTo(endX, endY)
-    ctx.stroke()
-    ctx.strokeStyle = strokeColor
-    drawCircle(startX, startY, strokeColor)
-    drawCircle(endX, endY, strokeActiveColor)
-}
-
-function getXCoordinate(position) {
-    return (position - 1) % columns * length + offset
-}
-
-function getYCoordinate(position) {
-    return Math.floor((position - 1) / columns) * length + offset
-}
-
-function drawCircle(x, y, color) {
-    ctx.strokeStyle = color
-    ctx.fillStyle = color
-    ctx.beginPath();
-    ctx.arc(x, y, circleRadius * scale, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fill()
-    ctx.stroke()
-    ctx.strokeStyle = strokeColor
-    ctx.strokeStyle = strokeColor
-}
-
-let currentNote = 1
-
-function goLeft(note, columns) {
-    const nextNote = note - 1
-    if (nextNote % columns !== 0) {
-        drawLine(note, note - 1, strokeColor)
-        currentNote = nextNote
+class Config {
+    constructor(minSize, scale) {
+        this.rows = 3
+        this.columns = 4
+        this.circleRadius = 8
+        this.scale = scale
+        this.length = Math.floor(minSize / this.columns) * this.scale
+        this.gridWith = this.length / this.scale * (this.columns - 1)
+        this.gridPadding = (minSize - this.gridWith) / 2
+        this.offset = (minSize - this.gridWith - this.gridPadding) * this.scale
+        this.inactiveColor = '#444'
+        this.strokeColor = 'white'
+        this.strokeActiveColor = '#fcbe24'
     }
 }
 
-function goRight(note, columns) {
-    const nextNote = note + 1
-    if (note % columns !== 0) {
-        drawLine(note, nextNote, strokeColor)
-        currentNote = nextNote
+class Board {
+    constructor(config, canvas) {
+        this.config = config
+        this.ctx = canvas.getContext("2d");
+        this.ctx.lineWidth = 4;
+    }
+
+    drawLine(from, to, color) {
+        this.ctx.strokeStyle = color;
+        const startX = this.getXCoordinate(from)
+        const startY = this.getYCoordinate(from)
+        const endX = this.getXCoordinate(to)
+        const endY = this.getYCoordinate(to)
+        this.ctx.beginPath()
+        this.ctx.moveTo(startX, startY)
+        this.ctx.lineTo(endX, endY)
+        this.ctx.stroke()
+        this.ctx.strokeStyle = this.config.strokeColor
+        this.drawCircle(startX, startY, this.config.strokeColor)
+        this.drawCircle(endX, endY, this.config.strokeActiveColor)
+    }
+
+    drawCircle(x, y, color) {
+        this.ctx.strokeStyle = color
+        this.ctx.fillStyle = color
+        this.ctx.beginPath();
+        this.ctx.arc(
+            x,
+            y,
+            this.config.circleRadius * this.config.scale,
+            0,
+            Math.PI * 2,
+            true
+        );
+        this.ctx.closePath();
+        this.ctx.fill()
+        this.ctx.stroke()
+        this.ctx.strokeStyle = this.config.strokeColor
+    }
+
+    getXCoordinate(position) {
+        return (position - 1) % this.config.columns * this.config.length + this.config.offset
+    }
+
+    getYCoordinate(position) {
+        return Math.floor((position - 1) / this.config.columns) * this.config.length + this.config.offset
     }
 }
 
-function goUp(note, columns) {
-    const nextNote = note - columns
-    if (nextNote > 0) {
-        drawLine(note, nextNote, strokeColor)
-        currentNote = nextNote
+class Game {
+    constructor(config, board) {
+        this.config = config
+        this.points = []
+        this.board = board
+    }
+
+    createBoard() {
+        for (let position = 1; position <= this.config.rows * this.config.columns; position++) {
+            this.points.push(new Point(position, this.config))
+            this.board.drawCircle(
+                this.board.getXCoordinate(position),
+                this.board.getYCoordinate(position),
+                this.config.inactiveColor
+            )
+        }
+        this.createLinks()
+        this.currentPoint = this.points[0]
+    }
+
+    createLinks() {
+        for (let point of this.points) {
+            if (point.canGoUp()) {
+                point.pointUp = this.points[point.position - this.config.columns - 1]
+            }
+            if (point.canGoRight()) {
+                point.pointRight = this.points[point.position]
+            }
+            if (point.canGoDown()) {
+                point.pointDown = this.points[point.position + this.config.columns - 1]
+            }
+            if (point.canGoLeft()) {
+                point.pointLeft = this.points[point.position - 2]
+            }
+        }
+    }
+
+    controls(key) {
+        switch (key) {
+            case "ArrowLeft":
+            case "a":
+                if (this.currentPoint.canGoLeft()) {
+                    this.board.drawLine(
+                        this.currentPoint.position,
+                        this.currentPoint.pointLeft.position,
+                        this.config.strokeColor
+                    )
+                    this.currentPoint = this.currentPoint.pointLeft
+                }
+                break;
+            case "ArrowRight":
+            case "d":
+                if (this.currentPoint.canGoRight()) {
+                    this.board.drawLine(
+                        this.currentPoint.position,
+                        this.currentPoint.pointRight.position,
+                        this.config.strokeColor
+                    )
+                    this.currentPoint = this.currentPoint.pointRight
+                }
+                break;
+            case "ArrowUp":
+            case "w":
+                if (this.currentPoint.canGoUp()) {
+                    this.board.drawLine(
+                        this.currentPoint.position,
+                        this.currentPoint.pointUp.position,
+                        this.config.strokeColor
+                    )
+                    this.currentPoint = this.currentPoint.pointUp
+                }
+                break;
+            case "ArrowDown":
+            case "s":
+                if (this.currentPoint.canGoDown()) {
+                    this.board.drawLine(
+                        this.currentPoint.position,
+                        this.currentPoint.pointDown.position,
+                        this.config.strokeColor
+                    )
+                    this.currentPoint = this.currentPoint.pointDown
+                }
+                break;
+        }
     }
 }
 
-function goDown(note, columns, rows) {
-    const nextNote = note + columns
-    if (nextNote <= rows * columns) {
-        drawLine(note, nextNote, strokeColor)
-        currentNote = nextNote
+class Point {
+    constructor(position, config) {
+        this.position = position
+        this.config = config
+        this.pointUp = null
+        this.pointRight = null
+        this.pointDown = null
+        this.pointLeft = null
+    }
+
+    canGoUp() {
+        return this.position - this.config.columns > 0
+    }
+
+    canGoRight() {
+        return this.position % this.config.columns !== 0
+    }
+
+    canGoDown() {
+        return (
+            this.position + this.config.columns
+            <= this.config.rows * this.config.columns
+        )
+    }
+
+    canGoLeft() {
+        return this.position - 1 % this.config.columns !== 0
     }
 }
 
-function controls(key) {
-    switch (key) {
-        case "ArrowLeft":
-        case "a":
-            goLeft(currentNote, columns)
-            break;
-        case "ArrowRight":
-        case "d":
-            goRight(currentNote, columns)
-            break;
-        case "ArrowUp":
-        case "w":
-            goUp(currentNote, columns)
-            break;
-        case "ArrowDown":
-        case "s":
-            goDown(currentNote, columns, rows)
-            break;
-    }
-}
+config = new Config(minSize, scale)
+game = new Game(config, new Board(config, canvas))
+game.createBoard()
 
 function pressKey(key) {
-    controls(key)
+    game.controls(key)
 }
 
 window.addEventListener('keydown', (event) => {
-    controls(event.key)
+    game.controls(event.key)
 })
