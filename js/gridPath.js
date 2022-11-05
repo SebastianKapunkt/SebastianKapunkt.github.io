2a32c69acc43302f386f114ca302234951932315
SebastianKapunkt <kindt.sebastian@outlook.com>
2022-11-05 22:07:48 +0100

    maybe its really just location

--- js/gridPath.js
+++ js/gridPath.js
@@ -397,7 +397,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let rows = 4
     let columns = 5
 
-    console.log(location)
+    const urlParams = new URLSearchParams();
+    if (urlParams.has("columns") && urlParams.has("rows")) {
+        let paramsRows = parseInt(urlParams.get("rows"))
+        let paramsColumns = parseInt(urlParams.get("columns"))
+        if (sizes.find(size => size.rows === paramsRows && size.columns === paramsColumns)) {
+            rows = paramsRows
+            columns = paramsColumns
+        }
+    }
     createNewGame(rows,columns)
 
     const settingsDialog = document.getElementById('settings-dialog')
